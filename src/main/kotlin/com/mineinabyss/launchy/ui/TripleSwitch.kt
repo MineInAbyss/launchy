cd0fdac43e7c9b7a7d4f381857a340bbec0eae7f
0ffz <dan.voznyy@gmail.com>
2022-02-19 23:45:37 -0500

    Fix Appimage

--- src/main/kotlin/com/mineinabyss/launchy/ui/TripleSwitch.kt
+++ src/main/kotlin/com/mineinabyss/launchy/ui/TripleSwitch.kt
@@ -1,10 +1,7 @@
 package com.mineinabyss.launchy.ui
 
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Check
