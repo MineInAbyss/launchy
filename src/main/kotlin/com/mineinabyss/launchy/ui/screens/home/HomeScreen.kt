807d907140a050fb2bd2b55a6815f61262e92e4c
Boy <sivertpaulsen2@gmail.com>
2024-03-03 12:28:58 +0100

    refactor: add 'add account' button if not logged in

--- src/main/kotlin/com/mineinabyss/launchy/ui/screens/home/HomeScreen.kt
+++ src/main/kotlin/com/mineinabyss/launchy/ui/screens/home/HomeScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
@@ -17,14 +19,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mineinabyss.launchy.LocalLaunchyState
 import com.mineinabyss.launchy.data.modpacks.ModpackInfo
+import com.mineinabyss.launchy.logic.Auth
 import com.mineinabyss.launchy.logic.Launcher
 import com.mineinabyss.launchy.state.ProfileState
 import com.mineinabyss.launchy.ui.elements.PlayerAvatar
 import com.mineinabyss.launchy.ui.screens.LocalModpackState
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen() {
     val state = LocalLaunchyState
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold { paddingValues ->
         Box(Modifier.padding(paddingValues)) {
             val profile = state.profile.currentProfile
@@ -38,22 +44,18 @@ fun HomeScreen() {
                     Modifier.padding(fabPadding).align(Alignment.BottomEnd),
                     verticalArrangement = Arrangement.spacedBy(fabPadding)
                 ) {
-                    if (profile != null) {
-
-                        FloatingActionButton(
-                            onClick = { },
-                            modifier = Modifier.size(fabSize),
-                        ) {
-                            PlayerAvatar(profile, Modifier.fillMaxSize())
-                        }
-                    }
                     FloatingActionButton(
-                        onClick = { },
-                        modifier = Modifier.size(fabSize),
+                        onClick = {
+                            coroutineScope.launch {
+                                //if (profile == null) Auth.authOrShowDialog(state.profile)
+                            }
+                        },
+                        modifier = Modifier.size(fabSize).border(1.dp, MaterialTheme.colorScheme.secondary, FloatingActionButtonDefaults.shape),
                         containerColor = MaterialTheme.colorScheme.secondaryContainer,
                         contentColor = MaterialTheme.colorScheme.secondary,
                     ) {
-                        Icon(Icons.Rounded.Settings, contentDescription = "Add modpack")
+                        profile?.let { PlayerAvatar(profile, Modifier.fillMaxSize()) }
+                            ?: Icon(Icons.Rounded.Add, contentDescription = "Add account")
                     }
                 }
             }
